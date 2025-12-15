8ca01c3c6110b2a320b03c4791570655feb61f8f
Heysem Katibi <heysem.katibi@useinsider.com>
2025-12-15 19:01:48 +0300

    feat: add insider and insiderwebview dependencies to the android project

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -43,8 +43,9 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.androidx.webkit)
+    implementation(libs.insider)
+    implementation(libs.insider.webView)
 }
+
 apply(from = "build-webpage.gradle.kts")
