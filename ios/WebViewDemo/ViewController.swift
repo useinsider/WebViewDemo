7abb1e4cc62801e5663893b1f449aabf5cde26b0
Heysem Katibi <heysem.katibi@useinsider.com>
2025-12-15 13:22:10 +0300

    feat: load the web page on the iOS project

--- ios/WebViewDemo/ViewController.swift
+++ ios/WebViewDemo/ViewController.swift
@@ -6,10 +6,25 @@
 //
 
 import UIKit
+import WebKit
+import InsiderMobile
+import InsiderWebView
 
 class ViewController: UIViewController {
+    @IBOutlet weak var webView: WKWebView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        if #available(iOS 16.4, *) {
+            webView.isInspectable = true
+        }
+
+        Insider.setupWebViewSDK(on: webView)
+
+        if let htmlPath = Bundle.main.path(forResource: "index", ofType: "html") {
+            let url = URL(fileURLWithPath: htmlPath)
+            webView.loadFileURL(url, allowingReadAccessTo: url.deletingLastPathComponent())
+        }
     }
 }
